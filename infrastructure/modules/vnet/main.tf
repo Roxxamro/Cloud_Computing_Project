426580eca1b100365d083b45dcb5086cf865199b
Remiprotin <remi.protin@student.junia.com>
2024-11-22 19:10:25 +0100

    feat (delagation) : ajout de la partie delegation.

--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -10,4 +10,12 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_prefixes
+
+delegation {
+    name = "myDelegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
